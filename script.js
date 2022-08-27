c1da5675b99bf25b7f613d948887be476fca5682
soysanti44 <105685445+soysanti44@users.noreply.github.com>
2022-08-27 10:23:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("mi primer proyecto en consola")
+var primernombre="santiago";
+var apellido="guzman";
+var nombrecompleto =primernombre.concat(apellido);
+console.log(nombrecompleto);
+
+var numero1 =49;
+var numero2 =78;
+var suma =numero1+numero2;
+console.log(suma);
+
+var numero3 =74;
+var numero4 =56;
+var resta =numero3-numero4;
+console.log(resta);
+
+var numero5 =32;
+var numero6 =73;
+var mutiplicasion =numero5*numero6;
+console.log(mutiplicasion)
+
+var numero7 =27;
+var numero8 =98;
+var division =numero7/numero8;
+console.log(division)
